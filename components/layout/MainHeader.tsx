c1b244180d5c8b2a0f7f44fd0ab69695b019d143
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2022-11-29 01:27:00 +0900

    style: main page header 스타일링

--- components/layout/MainHeader.tsx
+++ components/layout/MainHeader.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components/native';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 
 import {color, layout, font} from 'styles/Theme';
 
@@ -7,49 +7,109 @@ import {color, layout, font} from 'styles/Theme';
 const MainHeaderView: React.FC = () => {
   return (
     <>
-      <MainHeader.Container>
-        <MainHeader.Top.Container>
-          <MainHeader.Top.Counter>D+4</MainHeader.Top.Counter>
-          <View>
-            <MainHeader.TodayFeel.Text>
+      <Hd.Container>
+        <Hd.Feel.Container>
+          <Hd.Feel.Counter>D+4</Hd.Feel.Counter>
+          <Hd.Feel.Content.Container>
+            <Hd.Feel.Content.Text>
               오늘의 기분을 표현해보세요
-            </MainHeader.TodayFeel.Text>
-          </View>
-        </MainHeader.Top.Container>
-      </MainHeader.Container>
+            </Hd.Feel.Content.Text>
+            <Hd.Feel.Content.Icon />
+          </Hd.Feel.Content.Container>
+        </Hd.Feel.Container>
+        <Hd.Question.Container>
+          <Hd.Question.Title>하루문답</Hd.Question.Title>
+          <Hd.Question.Content.Container>
+            <Hd.Question.Content.Text>
+              요즘 가장 하고 싶은 게 뭐에요?
+            </Hd.Question.Content.Text>
+            <Hd.Question.Content.Btn>
+              <Hd.Question.Content.BtnText>
+                기록하기
+              </Hd.Question.Content.BtnText>
+            </Hd.Question.Content.Btn>
+          </Hd.Question.Content.Container>
+        </Hd.Question.Container>
+      </Hd.Container>
     </>
   );
 };
 
 // COMPONENT style
-const MainHeader = {
+const Hd = {
   Container: styled.View`
-    border: 1px solid red;
     background: #fff;
     border-bottom-left-radius: 15px;
     border-bottom-right-radius: 15px;
+    padding: 8px 20px;
+    padding-bottom: 32px;
   `,
-  Top: {
+  Feel: {
     Container: styled.View`
       flex-direction: row;
       justify-content: space-between;
+      align-items: center;
       height: ${layout.headerHeight};
     `,
     Counter: styled.Text`
       color: ${color.main.main};
       font-size: 24px;
-      font-weight: bold;
       ${font.sans.bold}
-      font-family: 'SpoqaHanSansNeo-Bold';
-      font-weight: 600;
     `,
+    Content: {
+      Container: styled.View`
+        flex-direction: row;
+        align-items: center;
+      `,
+      Text: styled.Text`
+        color: ${color.main.main};
+        font-size: 12px;
+        ${font.sans.bold}
+      `,
+      Icon: styled.View`
+        border-radius: 100px;
+        width: 28px;
+        height: 28px;
+        border: 2px dashed ${color.main.main};
+        margin-left: 12px;
+      `,
+    },
   },
-  TodayFeel: {
-    Text: styled.Text`
+  Question: {
+    Container: styled.View`
+      margin-top: 20px;
+    `,
+    Title: styled.Text`
       color: ${color.main.main};
       font-size: 12px;
       ${font.sans.bold}
     `,
+    Content: {
+      Container: styled.View`
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        margin-top: 8px;
+      `,
+      Text: styled.Text`
+        color: ${color.main.main};
+        font-size: 16px;
+        ${font.sans.medium}
+      `,
+      Btn: styled.TouchableOpacity`
+        width: 82px;
+        height: 28px;
+        background: ${color.main.main};
+        border-radius: 4px;
+        justify-content: center;
+        align-items: center;
+      `,
+      BtnText: styled.Text`
+        color: #fff;
+        font-size: 12px;
+        ${font.sans.medium}
+      `,
+    },
   },
 };
 
